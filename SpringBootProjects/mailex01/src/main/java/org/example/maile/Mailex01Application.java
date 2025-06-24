81e0d98b04fb9c6a2c4f7c9f655bc68d351d2438
dudtlr2331 <dudtlr102@naver.com>
2025-06-24 22:46:32 +0900

    mail 보내기 예제

---
+++ SpringBootProjects/mailex01/src/main/java/org/example/maile/Mailex01Application.java
@@ -0,0 +1,13 @@
+package org.example.maile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mailex01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mailex01Application.class, args);
+    }
+
+}
---
+++ SpringBootProjects/mailex01/src/test/java/org/example/maile/Mailex01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.maile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mailex01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
