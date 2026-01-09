3ba0af20c9e8c2fd19a9a0655d188c0345ffbd17
YS <dudtlr102@naver.com>
2026-01-09 18:29:37 +0900

    코드 수정

---
+++ algorithm/src/main/java/Backjoon11655.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Backjoon11655 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            // 대문자 처리
+            if (c >= 'A' && c <= 'Z') {
+                char shift = (char) (c + 13);
+                if (shift > 'Z') {
+                    shift = (char) (shift - 26);
+                }
+                sb.append(shift);
+            }
+            // 소문자 처리
+            else if (c >= 'a' && c <= 'z') {
+                char shift = (char) (c + 13);
+
+                if (shift > 'z') {
+                    shift = (char) (shift - 26);
+                }
+                sb.append(shift);
+            }
+            else {
+                sb.append(c);
+            }
+        }
+
+        System.out.println(sb.toString());
+        sc.close();
+    }
+}
